1cf29561c4c679db14611dd66924832087875b6a
ilich <itovar22@jaumebalmes.net>
2023-01-16 21:01:05 +0100

    subido archivos por roger

---
+++ invest.py
@@ -0,0 +1,8 @@
+"""
+2)Crear un arxiu de nom invest.py. Aquest programa ha de calcular el total del diners estalviats
+durant els pròxims 7 anys (sense variables).
+Es tenen uns diners inicials de 100€ i cada any s’hi incrementa un 1.1 durant 7 anys.
+
+"""
+
+print(100*1.1*7)
